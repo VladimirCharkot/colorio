91cf19bf7602aaf7636d3c4b8b57a95ed1c00337
dliendo <dario.liendo@gmail.com>
2024-08-05 20:53:30 -0300

    control sobre el disparo

--- tirador.gd
+++ tirador.gd
@@ -2,18 +2,38 @@ extends CharacterBody2D
 
 var proyectil = preload("res://proyectil.tscn")
 
-var shoot_force = 600;
-var direction_angle = 1.40
+var max_shoot_force = 2000;
+var shoot_force = 0;
+var direction_angle = 0;
+var shoot_force_step = 500;
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Animacion.play("desenfundar")
-	var disparado = proyectil.instantiate()
-	print(Vector2.from_angle(PI * direction_angle))
-	disparado.linear_velocity = Vector2.from_angle(PI * direction_angle) * shoot_force
-	add_child(disparado)
-	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_pressed("shoot") and shoot_force < max_shoot_force:
+		if (shoot_force + shoot_force_step) > max_shoot_force:
+			shoot_force = max_shoot_force
+		shoot_force += (shoot_force_step * delta)
+		print(shoot_force)
+		
+	if Input.is_action_just_released("shoot"):
+		shoot_projectile();
+		shoot_force = 0;
+	
+	if Input.is_action_pressed("angle_up"):
+		if direction_angle < 2:
+			direction_angle += 1 * delta
+		print(direction_angle)
+		
+	if Input.is_action_pressed("angle_down"):
+		if direction_angle > -2:
+			direction_angle -= 0.1 * delta
+		print(direction_angle)
+
+func shoot_projectile():
+	var disparado = proyectil.instantiate()
+	disparado.linear_velocity = Vector2.from_angle(PI * direction_angle) * shoot_force
+	add_child(disparado)
